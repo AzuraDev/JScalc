c89f71ca0e9bfab054e624b652516712eab6d278
Azura <azuradev@gmail.com>
2014-11-09 18:41:10 -0500

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,6 +1,7 @@
 javascript:
 
 var pr;
+var p3;
 var p2;
 var p1;
 var p;
@@ -13,10 +14,10 @@ function calc(e) {
     if (e.keyCode === 50) {
         num1 = prompt('Enter Num 1');
         num2 = prompt('Enter Num 2');
-        sum;
         p = '/';
         p1 = '-';
         p2 = '*';
+        p3 = '+';
         pr = prompt('Calc With');
         if (p === pr) {
             sum = num1 / num2;
@@ -24,6 +25,10 @@ function calc(e) {
             sum = num1 - num2;
         } else if (p2 === pr) {
             sum = num1 * num2;
+        } else if (p3 === pr) {
+            x = parseInt(num1);
+            y = parseInt(num2);
+            sum = x + y;
         }
     } else if (e.keyCode === 49) {
         var sum1 = alert(num1 + ' ' + pr + ' ' + num2 + ' = ' + sum);
