f7e1433ee3f22e5eb3801fb0ef6be6865528b02b
Azura <azuradev@gmail.com>
2014-11-10 23:26:22 -0500

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,6 +1,9 @@
 javascript:
 
 var pr;
+var p6;
+var p5;
+var p4;
 var p3;
 var p2;
 var p1;
@@ -14,13 +17,17 @@ document.onkeydown = calc;
 function calc(e) {
     if (e.keyCode === 50) {
         num1 = prompt('Enter Num 1');
-
-        num2 = prompt('Enter Num 2');
         p = '/';
         p1 = '-';
         p2 = '*';
         p3 = '+';
+        p4 = 'cos';
+        p5 = 'sin';
+        p6 = 'sqr';
         pr = prompt('Calc With');
+        if (x1 === 0) {
+            num2 = prompt('Enter Num 2');
+        }
         x = parseFloat(num1);
         y = parseFloat(num2);
         if (p === pr) {
@@ -31,9 +38,22 @@ function calc(e) {
             sum = x * y;
         } else if (p3 === pr) {
             sum = x + y;
+        } else if (p4 === pr) {
+            sum = Math.cos(x);
+            x1 = 1;
+        } else if (p5 === pr) {
+            sum = Math.sin(x);
+            x1 = 1;
+        } else if (p6 === pr) {
+            sum = Math.sqrt(num1);
+            x1 = 2;
         }
-        var sum1 = alert(num1 + ' ' + pr + ' ' + num2 + ' = ' + sum);
-    } else if (e.keyCode === 49) {
-        sum1 = alert(num1 + ' ' + pr + ' ' + num2 + ' = ' + sum);
+        if (x1 === 0) {
+            var sum1 = alert(num1 + ' ' + pr + ' ' + num2 + ' = ' + sum);
+        } else if (x1 === 1) {
+            sum1 = alert(pr + '(' + num1 + ')' + ' = ' + sum);
+        } else if (x1 === 2) {
+            sum1 = alert(pr + ' of ' + num1 + ' = ' + sum);
+        }
     }
 }
