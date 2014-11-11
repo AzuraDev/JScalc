3734832ba4d52f8415873a168532edf3ef92e640
Azura <azuradev@gmail.com>
2014-11-11 14:57:43 -0500

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,33 +1,30 @@
 javascript:
 
 var pr;
-var p6;
-var p5;
-var p4;
-var p3;
-var p2;
-var p1;
-var p;
+var p6 = 'sqr';
+var p5 = 'sin';
+var p4 = 'cos';
+var p3 = '+';
+var p2 = '*';
+var p1 = '-';
+var p = '/';
 var sum;
 var num2;
 var num1;
 var x1 = 0;
+var z = 0;
+/*Were The Variables Are Stored
+And Kept In The Script*/
 document.onkeydown = calc;
 
 function calc(e) {
     if (e.keyCode === 50) {
         num1 = prompt('Enter Num 1');
-        p = '/';
-        p1 = '-';
-        p2 = '*';
-        p3 = '+';
-        p4 = 'cos';
-        p5 = 'sin';
-        p6 = 'sqr';
         pr = prompt('Calc With');
-        if (x1 === 0) {
-            num2 = prompt('Enter Num 2');
-        }
+        num2 = prompt('Enter Num 2 ');
+        /*The Input Sequence, Logs What
+        Numbers And Signs You Entered*/
+        x1 = 0;
         x = parseFloat(num1);
         y = parseFloat(num2);
         if (p === pr) {
@@ -48,12 +45,18 @@ function calc(e) {
             sum = Math.sqrt(num1);
             x1 = 2;
         }
+        /*The Checking System And The Actual Calculator 
+        Checks And Calculates The Things That Were
+        Loged First Into The Calculator*/
         if (x1 === 0) {
             var sum1 = alert(num1 + ' ' + pr + ' ' + num2 + ' = ' + sum);
         } else if (x1 === 1) {
-            sum1 = alert(pr + '(' + num1 + ')' + ' = ' + sum);
+            sum1 = alert(pr + ' (' + num1 + ') ' + ' = ' + sum);
         } else if (x1 === 2) {
             sum1 = alert(pr + ' of ' + num1 + ' = ' + sum);
         }
+        /*Output System, Outputs The Things That Were Inputed
+        And The Things That Were Checked, This Is What
+        You See When You Get Your Answer*/
     }
 }
